e7a7a6deabf0f6f5423aafe42d1a2261b596dd4e
Kostegs <ksuslin@gmail.com>
2023-06-01 10:54:48 +0300

    Rotation Coroutine

--- Assets/Scripts/LerpRotation.cs
+++ Assets/Scripts/LerpRotation.cs
@@ -5,32 +5,37 @@ using UnityEngine;
 
 public class LerpRotation : MonoBehaviour
 {
-    private Quaternion _startRotation;
-    private Quaternion _targetRotation;
-
     private float _elapsedTime;
-    private float _rotationTime = 3;
-
-    private void Start()
-    {
-        _startRotation = transform.rotation;
-        _targetRotation = Quaternion.Euler(0f, 0f, -90f); // 90 degrees rotation along z-axis
-    }
 
     private void Update()
     {
-        float delta = _elapsedTime / _rotationTime;
-        
-        if (_elapsedTime < _rotationTime)
-        {
-            transform.rotation = Quaternion.Lerp(_startRotation, _targetRotation, delta);
-            _elapsedTime += Time.deltaTime;
-        }
-        else
-        {
-            transform.rotation = _targetRotation;
-        }
+        if (Input.GetKeyDown(KeyCode.Space))
+            StartCoroutine(LerpCoroutine());            
 
         // TODO - корутины - поворот в одну сторону, потом обратно
+    }
+
+    IEnumerator LerpCoroutine()
+    {
+        Quaternion _targetRotation = Quaternion.Euler(0f, 0f, -90f); // 90 degrees rotation along z-axis
+        yield return StartCoroutine(RotationCoroutine(transform.rotation, _targetRotation, 3));
+
+        _elapsedTime = 0;
+        _targetRotation = Quaternion.Euler(0f, 0f, 0f);
+        yield return StartCoroutine(RotationCoroutine(transform.rotation, _targetRotation, 3));
+    }
+
+    IEnumerator RotationCoroutine(Quaternion startRotation, Quaternion targetRotation, float _rotationTime)
+    {
+        _elapsedTime = 0;
+        
+        while (_elapsedTime < _rotationTime)
+        {
+            transform.rotation = Quaternion.Lerp(startRotation, targetRotation, _elapsedTime / _rotationTime);
+            _elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        
+        transform.rotation = targetRotation;
     }
 }
